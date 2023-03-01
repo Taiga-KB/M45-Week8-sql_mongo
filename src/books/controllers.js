2f28758cd0fb94e42e5c25ffd6ad98a49fee0107
K Burnett <bluemalek@gmail.com>
2023-03-01 12:09:13 +0000

    update: addbook w/error catch

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,5 +1,24 @@
+const Book = require("./model");
+
 const addBook = async (req, res) => {
-    res.status(201).json({message: "hello"});
+    try {
+        const book = await Book.create(req.body);
+
+        console.log(book);
+
+        // const successResponse = {
+        //     message: "Success",
+        //     newBook: book,
+        // };
+        res.status(201).json({message: "Success", newBook: book});
+    } catch (error) {
+        console.log(error);
+        // const errorResponse = {
+        //     errorMessage: error.message,
+        //     error: error,
+        // };
+        res.status(501).json({errorMessage: error.message, error: error});
+    }
 };
 
 module.exports = {
